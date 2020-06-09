d470e04686e76f35f8037ca874457a002bb28fcc
Ahmed Nasr Eldien <amednaser975@gmail.com>
2020-06-09 03:55:55 +0200

    Files are Modified

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 // Change Coloring Properties of Navbar On Scrolling
 $(window).scroll(function () {
-  console.log($(this).scrollTop())
+
   if ($(this).scrollTop() >= 40) {
     $(".navbar").css({
       backgroundColor: "#FFF",
@@ -28,26 +28,4 @@ $(window).scroll(function () {
     $('.navbar .navbar-nav li').css('paddingBottom','1.4rem');
     $('.navbar .navbar-nav:last-child').css('border-left-color','rgba(255, 255, 255, 0.2)');
   }
-})
-
-if ($(".navbar").width() < 700) {
-
-  $(".navbar").css({
-    backgroundColor: "#FFF",
-    boxShadow: "0px 6px 9px 0px rgba(0,0,0,.1)"
-  });
-  $(".navbar-brand img").attr("src", "images/logo-dark.png");
-  $(".navbar-brand,.navbar-collapse ul li a").css("color", "#333");
-
-} else {
-
-  $(".navbar").css({
-    backgroundColor: "transparent",
-    boxShadow: "none"
-  });
-  $(".navbar-brand img").attr("src", "images/logo-light.png");
-  $(".navbar-brand,.navbar-collapse ul li a").css("color", "#FFF");
-}
-$(".navbar-toggler span").click(function (){
-  $(".navbar").css("backgroundColor", "#333")
 })
