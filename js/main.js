d9fd6a249cb24baa2e759fbefba3e83e328c7459
Ahmed Nasr Eldien <amednaser975@gmail.com>
2020-06-15 04:21:34 +0200

    All Files Modified

--- js/main.js
+++ js/main.js
@@ -27,30 +27,9 @@ $(window).scroll(function () {
     $(".navbar-toggler span").css('backgroundColor','#FFF');
     $(".navbar-brand img").attr("src", "images/logo-light.png");
     $(".navbar-brand,.navbar-collapse ul li a").css("color", "#FFF");
-    // $('.navbar .navbar-nav li').css('paddingTop','1.4rem');
-    // $('.navbar .navbar-nav li').css('paddingBottom','1.4rem');
     $('.navbar .navbar-nav:last-child').css('border-left-color','rgba(255, 255, 255, 0.2)');
   }
 })
-
-// if ($(".navbar").width() < 700) {
-
-//   $(".navbar").css({
-//     backgroundColor: "#FFF",
-//     boxShadow: "0px 6px 9px 0px rgba(0,0,0,.1)"
-//   });
-//   $(".navbar-brand img").attr("src", "images/logo-dark.png");
-//   $(".navbar-brand,.navbar-collapse ul li a").css("color", "#333");
-
-// } else {
-
-//   $(".navbar").css({
-//     backgroundColor: "transparent",
-//     boxShadow: "none"
-//   });
-//   $(".navbar-brand img").attr("src", "images/logo-light.png");
-//   $(".navbar-brand,.navbar-collapse ul li a").css("color", "#FFF");
-// }
 $(".navbar-toggler").click(function (){
   $(".navbar").css({
     "backgroundColor": "#fff",
@@ -60,37 +39,19 @@ $(".navbar-toggler").click(function (){
   $(".navbar-brand,.navbar-collapse ul li a").css("color", "#333");
   $(".navbar-toggler span").css('backgroundColor','#333');
 })
+// Scroll To Top button
+var scrollToTop = $("#back-to-top");
+var offsetTopAbout = $('#about').offset().top;
+$(window).scroll(function() {
+  if ($(window).scrollTop() >= offsetTopAbout) {
+    if (scrollToTop.is(":hidden")) {
+      scrollToTop.css("display", "block");
+    }
+  } else {
+    scrollToTop.css("display", "none");
+  }
+});
+$("#back-to-top").click( function () {
 
-// $(window).on('scroll', function () {
-    
-    
-//   if ($(this).scrollTop() >= 100)
-//   {
-//       $('.navbar').css({
-
-//           backgroundColor:'#333',
-//           top: '0px',
-//           left: '0px'
-          
-//       });
-
-//   } else if ($(this).scrollTop() >= 0)
-//   {
-//       $('.navbar').css({
-
-//           backgroundColor:'transparnet',
-//           top: '0px',
-//           left: '0px'
-          
-//       });
-//   } else {
-//       $('.navbar').css({
-
-//           backgroundColor:'transparnet',
-//           top: '0px',
-//           left: '0px'
-          
-//       });
-//   }
-
-//  });
+  $('html, body').animate({scrollTop: 0}, 1000);
+})
